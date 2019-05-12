9c0a96e4e19d449f8c3b34c6a9dae9bff99ee771
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-12 22:53:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
-# your code goes here
+require "pry"
+
+def begins_with_r(args)
+  args.all? {|arg| arg.start_with?("r")}
+end
+
+def contain_a(args)
+  has_a = []
+  args.each {|arg| has_a << arg if arg.include?("a")}
+  has_a
+end
+
+def first_wa(args)
+  args.find {|arg| arg.start_with?("wa") if arg.instance_of? String}
+end
+
+def remove_non_strings(args)
+  args.delete_if {|arg| (arg.instance_of? String) == false}
+end
+
+def count_elements(data)
+  count_array = []
+end
+
+def merge_data(keys, data)
+  merged = []
+  keys.each do |hash1|
+    data.each do |hashes2|
+      hashes2.each do |person, attributes|
+        if hash1[:first_name] == person
+          merged << hash1.merge(attributes)
+        end
+      end
+    end
+  end
+  merged
+end
+
+def find_cool(data)
+  is_cool = []
+  
+  data.each do |person|
+    if person[:temperature] == "cool"
+      is_cool << person
+    end
+  end
+  is_cool
+end
+
+def organize_schools(schools)
+  schools_by_location = []
+
+  schools.each do |school|
+    if (schools_by_location.include?(schools[school][:location])) == false
+      schools_by_location << schools[school][:location]
+      binding.pry
+    end
+    
+  end
+end
+
+
