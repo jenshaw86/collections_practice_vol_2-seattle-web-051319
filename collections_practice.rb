4da94e5048eaf95efd69d6179e435a3941c621f3
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-12 23:29:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,6 +51,8 @@ def count_elements(names)
   count_array = []
   
   names.each do |name|
+    count_array.each do |element|
+      if element 
     hash = name
     hash[:count] = names.count(name)
     count_array << hash
