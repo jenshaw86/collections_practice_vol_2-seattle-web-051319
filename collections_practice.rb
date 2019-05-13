2274153a80aff49460c36bca00562e2cf5e68bae
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-13 00:21:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,7 +62,7 @@ def organize_schools(schools)
       schools_by_location << info[:location] => [school]
     else
       schools_by_location.each |location|
-        
+        binding.pry
       end
       
     end
