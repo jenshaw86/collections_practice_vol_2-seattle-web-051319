cb4994621ddc8ec2a6daa36091c1f8de44e45f16
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-13 00:24:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,7 +62,7 @@ def organize_schools(schools)
       schools_by_location[info[:location]] = [school]
     else
       schools_by_location.each do |city, schools|
-        schools_by_location[city] << 
+        schools_by_location[city] << school
         binding.pry
       end
     end
